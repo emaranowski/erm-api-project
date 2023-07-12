ccc2e3ab545c0531d0c184058a5dc5f32291ab0e
Erica Maranowski <emaranowski@gmail.com>
2023-07-12 11:52:10 -0700

    Tidied the updated version of membership mig, mod, seed files after fixing Render deployment (had to drop meetup_schema from Render db, then re-deploy)

--- backend/db/models/membership.js
+++ backend/db/models/membership.js
@@ -1,74 +1,4 @@
-// 'use strict';
-
-// const { Model, Validator } = require('sequelize');
-
-// module.exports = (sequelize, DataTypes) => {
-//   class Membership extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-
-//       // one membership belongsTo one user
-//       // one user hasMany memberships
-//       Membership.belongsTo(
-//         models.User,
-//         { foreignKey: 'userId' }
-//       );
-
-//       // one membership belongsTo one group
-//       // one group hasMany memberships
-//       Membership.belongsTo(
-//         models.Group,
-//         { foreignKey: 'groupId' }
-//       );
-
-//     }
-//   }
-//   Membership.init({
-//     id: { // added to init(), since there aare multiple FKs; want PK to be 'id'
-//       type: DataTypes.INTEGER, // do not want auto-assigned PK based on combo of FK col names
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true
-//     },
-//     userId: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     groupId: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     status: {
-//       type: DataTypes.ENUM('pending', 'rejected', 'approved'),
-//       allowNull: true
-//     },
-//     // status: {
-//     //   type: DataTypes.ENUM('pending', 'rejected', 'approved'),
-//     //   allowNull: true,
-//     //   // defaultValue: 'pending'
-//     // },
-//   }, {
-//     sequelize,
-//     modelName: 'Membership',
-//     defaultScope: {
-//       attributes: {
-//         exclude: ['createdAt', 'updatedAt']
-//       }
-//     }
-//   });
-//   return Membership;
-// };
-
-
-
-
 // UPDATED:
-
-
 
 'use strict';
 
@@ -135,3 +65,70 @@ module.exports = (sequelize, DataTypes) => {
   });
   return Membership;
 };
+
+// ORIG:
+
+// 'use strict';
+
+// const { Model, Validator } = require('sequelize');
+
+// module.exports = (sequelize, DataTypes) => {
+//   class Membership extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+
+//       // one membership belongsTo one user
+//       // one user hasMany memberships
+//       Membership.belongsTo(
+//         models.User,
+//         { foreignKey: 'userId' }
+//       );
+
+//       // one membership belongsTo one group
+//       // one group hasMany memberships
+//       Membership.belongsTo(
+//         models.Group,
+//         { foreignKey: 'groupId' }
+//       );
+
+//     }
+//   }
+//   Membership.init({
+//     id: { // added to init(), since there aare multiple FKs; want PK to be 'id'
+//       type: DataTypes.INTEGER, // do not want auto-assigned PK based on combo of FK col names
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true
+//     },
+//     userId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//     },
+//     groupId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//     },
+//     status: {
+//       type: DataTypes.ENUM('pending', 'rejected', 'approved'),
+//       allowNull: true
+//     },
+//     // status: {
+//     //   type: DataTypes.ENUM('pending', 'rejected', 'approved'),
+//     //   allowNull: true,
+//     //   // defaultValue: 'pending'
+//     // },
+//   }, {
+//     sequelize,
+//     modelName: 'Membership',
+//     defaultScope: {
+//       attributes: {
+//         exclude: ['createdAt', 'updatedAt']
+//       }
+//     }
+//   });
+//   return Membership;
+// };
--- backend/db/seeders/20230711005902-memberships.js
+++ backend/db/seeders/20230711005902-memberships.js
@@ -1,53 +1,4 @@
-// 'use strict';
-
-// const { Membership } = require('../models');
-// const bcrypt = require('bcryptjs');
-
-// let options = {};
-// if (process.env.NODE_ENV === 'production') {
-//   options.schema = process.env.SCHEMA; // define schema in options obj
-// }
-
-// /** @type {import('sequelize-cli').Migration} */
-// module.exports = {
-//   async up(queryInterface, Sequelize) {
-
-//     await Membership.bulkCreate([
-//       {
-//         userId: 1,
-//         groupId: 1,
-//         status: 'pending'
-//       },
-//       {
-//         userId: 2,
-//         groupId: 2,
-//         status: 'rejected'
-//       },
-//       {
-//         userId: 3,
-//         groupId: 3,
-//         status: 'approved'
-//       },
-//     ], { validate: true });
-
-//   },
-
-//   async down(queryInterface, Sequelize) {
-
-//     options.tableName = 'Memberships';
-//     const Op = Sequelize.Op;
-//     return queryInterface.bulkDelete(options, {
-//       userId: {
-//         [Op.in]: [1, 2, 3] // added 4
-//       } // may need to add combos of userId & groupId?
-//     }, {});
-
-//   }
-// };
-
-
-
-// UPDATED
+// UPDATED:
 
 'use strict';
 
@@ -100,3 +51,53 @@ module.exports = {
 
   }
 };
+
+
+// ORIG:
+
+// 'use strict';
+
+// const { Membership } = require('../models');
+// const bcrypt = require('bcryptjs');
+
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+//   options.schema = process.env.SCHEMA; // define schema in options obj
+// }
+
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+
+//     await Membership.bulkCreate([
+//       {
+//         userId: 1,
+//         groupId: 1,
+//         status: 'pending'
+//       },
+//       {
+//         userId: 2,
+//         groupId: 2,
+//         status: 'rejected'
+//       },
+//       {
+//         userId: 3,
+//         groupId: 3,
+//         status: 'approved'
+//       },
+//     ], { validate: true });
+
+//   },
+
+//   async down(queryInterface, Sequelize) {
+
+//     options.tableName = 'Memberships';
+//     const Op = Sequelize.Op;
+//     return queryInterface.bulkDelete(options, {
+//       userId: {
+//         [Op.in]: [1, 2, 3] // added 4
+//       } // may need to add combos of userId & groupId?
+//     }, {});
+
+//   }
+// };
