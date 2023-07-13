b30903439a1e09ab42a594b0a22408a110630316
Erica Maranowski <emaranowski@gmail.com>
2023-07-12 18:30:57 -0700

    Updated ENUM values for group type in mig, mod, seed

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -1,3 +1,5 @@
+// UPDATED:
+
 'use strict';
 
 const { Model, Validator } = require('sequelize');
@@ -87,7 +89,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
     },
     type: {
-      type: DataTypes.ENUM('outdoors', 'food', 'board games'),
+      type: DataTypes.ENUM('Online', 'In person'), // updated vals
       allowNull: false,
     },
     private: {
@@ -113,3 +115,122 @@ module.exports = (sequelize, DataTypes) => {
   });
   return Group;
 };
+
+
+// ORIG:
+
+// 'use strict';
+
+// const { Model, Validator } = require('sequelize');
+
+// module.exports = (sequelize, DataTypes) => {
+//   class Group extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+
+//       // one user/organizer hasMany groups
+//       // one group belongsTo one user/organizer
+//       Group.belongsTo(
+//         models.User,
+//         { foreignKey: 'organizerId' } // may need to alias? like.. { foreignKey: 'currentTeamId', as: 'TeamRoster' }
+//       );
+
+//       // one membership belongsTo one group
+//       // one group hasMany memberships
+//       Group.hasMany(
+//         models.Membership,
+//         { foreignKey: 'groupId' }
+//       );
+
+//       // one group hasMany venues
+//       // one venue belongsTo one group
+//       Group.hasMany(
+//         models.Venue,
+//         { foreignKey: 'groupId' }
+//       );
+
+//       // one groupimage belongsTo one group
+//       // one group hasMany groupimages
+//       Group.hasMany(
+//         models.GroupImage,
+//         { foreignKey: 'groupId' }
+//       );
+
+//       // one group hasMany events
+//       // one event belongsTo one group
+//       Group.hasMany(
+//         models.Event,
+//         { foreignKey: 'groupId' }
+//       );
+
+//       // one user belongsToMany groups ?
+//       // one group belongsToMany users ?
+//       Group.belongsToMany(
+//         models.User,
+//         {
+//           through: models.Membership,
+//           foreignKey: 'groupId',
+//           otherKey: 'userId'
+//         }
+//       );
+
+//       // ?
+//       // many to many: venues-to-groups, via events
+//       // one venue belongsToMany groups
+//       // one group belongsToMany venues
+//       Group.belongsToMany(
+//         models.Venue,
+//         {
+//           through: models.Event,
+//           foreignKey: 'groupId',
+//           otherKey: 'venueId'
+//         }
+//       );
+
+//     }
+//   }
+//   Group.init({
+//     organizerId: {
+//       type: DataTypes.INTEGER, // add ref to organizerId/userId in migration, not model
+//       allowNull: false,
+//     },
+//     name: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       unique: true
+//     },
+//     about: {
+//       type: DataTypes.TEXT,
+//       allowNull: false,
+//     },
+//     type: {
+//       type: DataTypes.ENUM('outdoors', 'food', 'board games'),
+//       allowNull: false,
+//     },
+//     private: {
+//       type: DataTypes.BOOLEAN,
+//       allowNull: false,
+//     },
+//     city: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     state: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     }
+//   }, {
+//     sequelize,
+//     modelName: 'Group',
+//     // defaultScope: { // removed this dscope
+//     //   attributes: { // so createdAt & updatedAt show in Get All Groups
+//     //     exclude: ['createdAt', 'updatedAt']
+//     //   }
+//     // }
+//   });
+//   return Group;
+// };
--- backend/db/seeders/20230710232302-groups.js
+++ backend/db/seeders/20230710232302-groups.js
@@ -1,3 +1,5 @@
+// UPDATED:
+
 'use strict';
 
 const { Group } = require('../models');
@@ -17,7 +19,7 @@ module.exports = {
         organizerId: 1, // prob correct syntax for FK val
         name: 'Hikers',
         about: 'A group of hikers who love getting outdoors.',
-        type: 'outdoors', // may not be correct ENUM syntax
+        type: 'In person', // may not be correct ENUM syntax
         private: false,
         city: 'Portland',
         state: 'OR'
@@ -26,7 +28,7 @@ module.exports = {
         organizerId: 2, // prob correct syntax for FK val
         name: 'Hobby Chefs',
         about: 'Culinary creatives who want to share and try new recipes together.',
-        type: 'food', // may not be correct ENUM syntax
+        type: 'In person', // may not be correct ENUM syntax
         private: true,
         city: 'Portland',
         state: 'OR'
@@ -35,7 +37,7 @@ module.exports = {
         organizerId: 3, // prob correct syntax for FK val
         name: 'Board Game Fans',
         about: 'Folks who love playing and making board games.',
-        type: 'board games', // may not be correct ENUM syntax
+        type: 'Online', // may not be correct ENUM syntax
         private: false,
         city: 'Portland',
         state: 'OR'
@@ -56,3 +58,65 @@ module.exports = {
 
   }
 };
+
+
+// ORIG:
+
+// 'use strict';
+
+// const { Group } = require('../models');
+// const bcrypt = require('bcryptjs');
+
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+//   options.schema = process.env.SCHEMA; // define schema in options obj
+// }
+
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+
+//     await Group.bulkCreate([
+//       {
+//         organizerId: 1, // prob correct syntax for FK val
+//         name: 'Hikers',
+//         about: 'A group of hikers who love getting outdoors.',
+//         type: 'outdoors', // may not be correct ENUM syntax
+//         private: false,
+//         city: 'Portland',
+//         state: 'OR'
+//       },
+//       {
+//         organizerId: 2, // prob correct syntax for FK val
+//         name: 'Hobby Chefs',
+//         about: 'Culinary creatives who want to share and try new recipes together.',
+//         type: 'food', // may not be correct ENUM syntax
+//         private: true,
+//         city: 'Portland',
+//         state: 'OR'
+//       },
+//       {
+//         organizerId: 3, // prob correct syntax for FK val
+//         name: 'Board Game Fans',
+//         about: 'Folks who love playing and making board games.',
+//         type: 'board games', // may not be correct ENUM syntax
+//         private: false,
+//         city: 'Portland',
+//         state: 'OR'
+//       },
+//     ], { validate: true });
+
+//   },
+
+//   async down(queryInterface, Sequelize) {
+
+//     options.tableName = 'Groups';
+//     const Op = Sequelize.Op;
+//     return queryInterface.bulkDelete(options, {
+//       name: {
+//         [Op.in]: ['Hikers', 'Hobby Chefs', 'Board Game Fans']
+//       }
+//     }, {});
+
+//   }
+// };
