9242966fdeb1e9349516b0c1fd2c5104ce9943de
Erica Maranowski <emaranowski@gmail.com>
2023-07-28 14:56:09 -0700

    Add dropdown menu, but not fully functional

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,32 +1,106 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
 
-    const logout = (e) => {
-        e.preventDefault();
-        dispatch(sessionActions.logout());
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (!ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
     };
 
-    const ulClassName = "profile-dropdown";
+    document.addEventListener('click', closeMenu);
 
-    return (
-        <>
-            <button>
-                <i className="fas fa-user-circle" />
-            </button>
-            <ul className="profile-dropdown">
-                <li>{user.username}</li>
-                <li>{user.firstName} {user.lastName}</li>
-                <li>{user.email}</li>
-                <li>
-                    <button onClick={logout}>Log Out</button>
-                </li>
-            </ul>
-        </>
-    );
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+  return (
+    <>
+      <button onClick={openMenu}>
+        <i className="fas fa-user-circle" />
+      </button>
+      <ul className={ulClassName} ref={ulRef}>
+        <li>{user.username}</li>
+        <li>{user.firstName} {user.lastName}</li>
+        <li>{user.email}</li>
+        <li>
+          <button onClick={logout}>Log Out</button>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default ProfileButton;
+
+
+
+
+// also good: "fa-solid fa-user"
+// #e63a6a
+// #610c24
+// #e6e6e6
+// #212121
+// blues
+// #00798a
+// #008fa3
+
+/*
+<div style={{ color: "#00798a", fontSize: "18px" }}>
+    <i className="fa-regular fa-face-smile"></i>
+</div>
+*/
+
+// ORIG
+
+// import React, { useState } from "react";
+// import { useDispatch } from 'react-redux';
+// import * as sessionActions from '../../store/session';
+
+// function ProfileButton({ user }) {
+//     const dispatch = useDispatch();
+
+//     const logout = (e) => {
+//         e.preventDefault();
+//         dispatch(sessionActions.logout());
+//     };
+
+//     const ulClassName = "profile-dropdown";
+
+//     return (
+//         <>
+//             <button>
+//                 <i className="fas fa-user-circle" />
+//             </button>
+//             <ul className="profile-dropdown">
+//                 <li>{user.username}</li>
+//                 <li>{user.firstName} {user.lastName}</li>
+//                 <li>{user.email}</li>
+//                 <li>
+//                     <button onClick={logout}>Log Out</button>
+//                 </li>
+//             </ul>
+//         </>
+//     );
+// }
+
+// export default ProfileButton;
