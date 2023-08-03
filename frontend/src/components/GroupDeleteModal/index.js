013d0ab45cfedfbef04fb0c5d6bbf5037c61185a
Erica Maranowski <emaranowski@gmail.com>
2023-08-02 23:09:04 -0700

    Add delete group modal (close & redirects on deletion)

--- frontend/src/components/GroupDeleteModal/index.js
+++ frontend/src/components/GroupDeleteModal/index.js
@@ -31,26 +31,73 @@ function GroupDeleteModal({ groupId }) {
     //   });
 
     // successfully deletes, but does not close modal
-    try {
-      const res = await dispatch(deleteGroupThunk(groupId)); // VS Code gives note about not needing 'await', but it IS needed here
-      console.log(`*** in group DELETE modal try, res is: ***`, res)
-      if (res.message === 'Successfully deleted') {
-        setErrors({});
-        closeModal();
+    // try {
+    //   const res = await dispatch(deleteGroupThunk(groupId)); // VS Code gives note about not needing 'await', but it IS needed here
+    //   console.log(`*** in group DELETE modal try, res is: ***`, res)
+    //   if (res.message === 'Successfully deleted') {
+    //     setErrors({});
+    //     closeModal();
+    //     history.push(`/groups/`);
+    //   } else {
+    //     return res;
+    //   }
+    // } catch (res) { // if exception in above code, run .catch()
+    //   console.log(`*** in group DELETE modal catch, res is: ***`, res) // TypeError: Failed to execute 'json' on 'Response': body stream already read
+    //   const data = await res.json(); // get data from db
+    //   console.log(`*** in group DELETE modal catch, data is: ***`, data) // TypeError: Failed to execute 'json' on 'Response': body stream already read
+    //   if (data && data.errors) { // if errors from db
+    //     setErrors(data.errors); // setErrors
+    //   }
+    // };
+
+    setErrors({});
+
+
+    // try {
+    //   const res = await dispatch(deleteGroupThunk(groupId)); // VS Code gives note about not needing 'await', but it IS needed here
+    //   console.log(`*** in group DELETE modal try, res is: ***`, res)
+    //   if (res.message === 'Successfully deleted') {
+    //     history.push(`/groups/`);
+    //   } else {
+    //     return res;
+    //   }
+    // } catch (res) { // if exception in above code, run .catch()
+    //   console.log(`*** in group DELETE modal catch, res is: ***`, res) // TypeError: Failed to execute 'json' on 'Response': body stream already read
+    //   const data = await res.json(); // get data from db
+    //   console.log(`*** in group DELETE modal catch, data is: ***`, data) // TypeError: Failed to execute 'json' on 'Response': body stream already read
+    //   if (data && data.errors) { // if errors from db
+    //     setErrors(data.errors); // setErrors
+    //   }
+    // };
+
+
+    return dispatch(deleteGroupThunk(groupId))
+      .then(closeModal)
+      .then(() => {
         history.push(`/groups/`);
-      } else {
-        return res;
-      }
-    } catch (res) { // if exception in above code, run .catch()
-      console.log(`*** in group DELETE modal catch, res is: ***`, res) // TypeError: Failed to execute 'json' on 'Response': body stream already read
-      const data = await res.json(); // get data from db
-      console.log(`*** in group DELETE modal catch, data is: ***`, data) // TypeError: Failed to execute 'json' on 'Response': body stream already read
-      if (data && data.errors) { // if errors from db
-        setErrors(data.errors); // setErrors
-      }
-    };
+      })
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) {
+          setErrors(data.errors);
+        }
+      });
 
   };
+
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setErrors({});
+  //   return dispatch(sessionActions.login({ credential, password }))
+  //     .then(closeModal)
+  //     .catch(async (res) => {
+  //       const data = await res.json();
+  //       if (data && data.errors) {
+  //         setErrors(data.errors);
+  //       }
+  //     });
+  // };
 
   return (
     <>
