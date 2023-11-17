6cb04af166b7e714960ea725ea0bf6e13ec06280
Erica Maranowski <128769940+emaranowski@users.noreply.github.com>
2023-11-16 23:21:11 -0800

    OpenModalButtonSignupHome: cleanup

--- frontend/src/components/OpenModalButtonSignupHome/index.js
+++ frontend/src/components/OpenModalButtonSignupHome/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useModal } from "../../context/Modal";
 import './OpenModalButtonSignupHome.css';
 
-function OpenModalButtonSignupHome({
+export default function OpenModalButtonSignupHome({
   modalComponent, // component to render inside the modal
   buttonText, // text of the button that opens the modal
   onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
@@ -21,31 +21,4 @@ function OpenModalButtonSignupHome({
       {buttonText}
     </button>
   );
-}
-
-export default OpenModalButtonSignupHome;
-
-// ORIG
-
-// import React from "react";
-// import { useModal } from "../../context/Modal";
-// import './OpenModalButton.css';
-
-// function OpenModalButton({
-//   modalComponent, // component to render inside the modal
-//   buttonText, // text of the button that opens the modal
-//   onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
-//   onModalClose, // optional: callback function that will be called once the modal is closed
-// }) {
-//   const { setModalContent, setOnModalClose } = useModal();
-
-//   const onClick = () => {
-//     if (typeof onButtonClick === "function") onButtonClick();
-//     if (typeof onModalClose === "function") setOnModalClose(onModalClose);
-//     setModalContent(modalComponent);
-//   };
-
-//   return <button onClick={onClick}>{buttonText}</button>;
-// }
-
-// export default OpenModalButton;
+};
