7eea6af0c8d034752e43d206be40c322451ed36e
Erica Maranowski <128769940+emaranowski@users.noreply.github.com>
2023-11-16 21:47:58 -0800

    GroupDeleteModal: cleanup

--- frontend/src/components/GroupDeleteModal/index.js
+++ frontend/src/components/GroupDeleteModal/index.js
@@ -1,68 +1,20 @@
-import React, { useEffect, useState } from "react";
-import * as sessionActions from "../../store/session"; // sessionActions is obj
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
-import { useParams } from 'react-router-dom';
 import { deleteGroupThunk } from "../../store/groups";
 import { useHistory } from "react-router-dom";
 import "./GroupDeleteModal.css";
 
-function GroupDeleteModal({ groupId }) {
+export default function GroupDeleteModal({ groupId }) {
   const dispatch = useDispatch();
   const history = useHistory();
   const { closeModal } = useModal();
-  // const { groupId } = useParams();
-
   const [errors, setErrors] = useState('');
 
   const deleteGroup = async (e) => {
     e.preventDefault();
 
     setErrors({});
-
-    // return dispatch(sessionActions.login({ credential, password }))
-    //   .then(closeModal)
-    //   .catch(async (res) => {
-    //     const data = await res.json();
-    //     if (data && data.errors) {
-    //       setErrors(data.errors);
-    //     }
-    //   });
-
-    // successfully deletes, but does not close modal
-    // try {
-    //   const res = await dispatch(deleteGroupThunk(groupId)); // VS Code gives note about not needing 'await', but it IS needed here
-    //   if (res.message === 'Successfully deleted') {
-    //     setErrors({});
-    //     closeModal();
-    //     history.push(`/groups/`);
-    //   } else {
-    //     return res;
-    //   }
-    // } catch (res) { // if exception in above code, run .catch()
-    //   const data = await res.json(); // get data from db
-    //   if (data && data.errors) { // if errors from db
-    //     setErrors(data.errors); // setErrors
-    //   }
-    // };
-
-    setErrors({});
-
-
-    // try {
-    //   const res = await dispatch(deleteGroupThunk(groupId)); // VS Code gives note about not needing 'await', but it IS needed here
-    //   if (res.message === 'Successfully deleted') {
-    //     history.push(`/groups/`);
-    //   } else {
-    //     return res;
-    //   }
-    // } catch (res) { // if exception in above code, run .catch()
-    //   const data = await res.json(); // get data from db
-    //   if (data && data.errors) { // if errors from db
-    //     setErrors(data.errors); // setErrors
-    //   }
-    // };
-
 
     return dispatch(deleteGroupThunk(groupId))
       .then(closeModal)
@@ -75,22 +27,7 @@ function GroupDeleteModal({ groupId }) {
           setErrors(data.errors);
         }
       });
-
   };
-
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   setErrors({});
-  //   return dispatch(sessionActions.login({ credential, password }))
-  //     .then(closeModal)
-  //     .catch(async (res) => {
-  //       const data = await res.json();
-  //       if (data && data.errors) {
-  //         setErrors(data.errors);
-  //       }
-  //     });
-  // };
 
   return (
     <>
@@ -111,7 +48,5 @@ function GroupDeleteModal({ groupId }) {
         </button>
       </div>
     </>
-  );
-}
-
-export default GroupDeleteModal;
+  )
+};
