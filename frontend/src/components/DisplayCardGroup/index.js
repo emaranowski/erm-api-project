931cedebb7d888aadb35b06dfb161d4463ac09e8
Erica Maranowski <128769940+emaranowski@users.noreply.github.com>
2023-11-16 21:28:24 -0800

    DisplayCardGroup: cleanup comments

--- frontend/src/components/DisplayCardGroup/index.js
+++ frontend/src/components/DisplayCardGroup/index.js
@@ -1,55 +1,22 @@
 import { Link } from 'react-router-dom';
-// import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-// import { getAllGroupsThunk } from '../../store/groups';
 import './DisplayCardGroup.css';
 
 export default function DisplayCardGroup({ group }) {
-
-  /////////////////////////// NUM EVENTS
-  const groupIdAsNum = group.id;
+  const groupId = group.id;
   const allEvents = useSelector(state => state.events.allEvents);
 
+  /////////////// GET NUM EVENTS ///////////////
+  // if 1 event: display text is singular 'event'
+  // if 0 or 2+ events: display text is plural 'events'
   let numEvents;
   if (Object.values(allEvents).length) {
-    let allEventsArr = Object.values(allEvents);
-
-    let eventsByThisGroup = allEventsArr.filter(eventObj => {
-      return eventObj.groupId === groupIdAsNum;
+    const allEventsArr = Object.values(allEvents);
+    const eventsForGroup = allEventsArr.filter(eventObj => {
+      return eventObj.groupId === groupId;
     });
-
-    numEvents = eventsByThisGroup.length;
+    numEvents = eventsForGroup.length;
   };
-
-
-  // const groupPulled = useSelector(state => state.groups.singleGroup ? state.groups.singleGroup : {}); // {}
-  // const groupPulledImages = group.GroupImages;
-
-
-  // let previewImageURL;
-  // let previewImages;
-  // if (groupPulledImages.length !== undefined) {
-  //   previewImages = groupPulledImages.filter(image => {
-  //     return image.preview === true;
-  //   })
-  //   // previewImageURL = previewImages[0].url; // orig
-  //   previewImageURL = previewImages[previewImages.length - 1].url;
-
-
-  // WORKING -- V2
-  // const group2 = useSelector(state => state.groups.singleGroup ? state.groups.singleGroup : {}); // {}
-  //   const group2Images = useSelector(state => state.groups.singleGroup.GroupImages ? state.groups.singleGroup.GroupImages : []); // {}
-
-  //   let previewImageURL;
-  //   let previewImages;
-  //   if (group2Images.length) {
-  //     previewImages = group2Images.filter(image => {
-  //       return image.preview === true;
-  //     })
-  //     // previewImageURL = previewImages[0].url; // orig
-  //     previewImageURL = previewImages[previewImages.length - 1].url;
-  //   }
-  // }
 
   return (
     <>
