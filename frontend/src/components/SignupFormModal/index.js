095bb88d890eace3ffe1886a90f96e4f947633e3
Erica Maranowski <128769940+emaranowski@users.noreply.github.com>
2023-11-16 23:38:04 -0800

    SignupFormModal: cleanup

--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -19,14 +19,6 @@ export default function SignupFormModal() {
 
   useEffect(() => {
 
-    if (email.length &&
-      username.length >= 4 &&
-      firstName.length &&
-      lastName.length &&
-      password.length <= 6 &&
-      confirmPassword.length <= 6
-    ) setDisabled(false);
-
     if (!email.length) setDisabled(true);
     if (!username.length) setDisabled(true);
     if (username.length < 4) setDisabled(true);
@@ -36,6 +28,14 @@ export default function SignupFormModal() {
     if (password.length < 6) setDisabled(true);
     if (!confirmPassword.length) setDisabled(true);
     if (confirmPassword.length < 6) setDisabled(true);
+
+    if (email.length &&
+      username.length >= 4 &&
+      firstName.length &&
+      lastName.length &&
+      password.length >= 6 &&
+      confirmPassword.length >= 6
+    ) setDisabled(false);
 
   }, [email, username, firstName, lastName, password, confirmPassword]);
 
@@ -61,7 +61,7 @@ export default function SignupFormModal() {
         });
     }
     return setErrors({
-      confirmPassword: "Confirm Password field must be the same as the Password field"
+      confirmPassword: "Passwords must match"
     });
   };
 
